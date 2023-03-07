3daf2ae92e588fd52b5bfbac612c68092c925201
adfar <adf5092@gmail.com>
2023-03-07 09:07:24 -0600

    initial commit

---
+++ eda.py
@@ -0,0 +1,8 @@
+#%%
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
